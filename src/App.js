4041feca936e0eab4d70d4174a6eb3e9c269963c
sulenchy <sulaimanabiodun172@gmail.com>
2020-03-08 18:06:13 +0100

    reinstall dependencies

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <img className="" src={require('./profile.jpg')} alt="Display" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
-          Blessing Krofegha
+          Abudu Abiodun Sulaiman (@sulenchy)
         </div>
         <p className="text-gray-700 text-base">
         Hello! I am a meticulous software engineer who loves using technology to be creative, innovative and solve real problems. I have 3+ years experience in software engineering. I love getting things better using code, making researches and automation (of processes, of business processes).<span></span>😜
@@ -14,8 +14,7 @@ function App() {
       </div>
       <div className="px-6 py-4">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Software Engineer</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Writter</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Public Speaker</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Writer</span>
       </div>
     </div>
   );
