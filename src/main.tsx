31c71320c44eee9f549bd3a5c887a18914bcc7e5
sulenchy <sulaimanabiodun172@gmail.com>
2020-12-31 16:04:43 +0100

    update the social meida section

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ export default function main() {
       </header>
 
       <section id="welcome-section" className="lg:h-screen flex items-center lg:flex-row flex-col pt-20">
-        <main className="w-1/2 mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 align-center">
+        <main className="lg:w-1/2 mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 align-center">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Hey, I'm Sulenchy. I code with </span>
@@ -125,11 +125,11 @@ export default function main() {
       <section id="social-media" className="lg:h-screen flex items-center flex-col pt-40">
         <h1 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">Let's work together...</h1>
         <p>How do you take your coffee?</p>
-        <div id="social-link">
-          <a href="https://web.facebook.com/sulenchy/" target="_blank">Facebook</a>
-          <a href="https://github.com/sulenchy" target="_blank">Github</a>
-          <a href="https://twitter.com/ababsul" target="_blank">Twitter</a>
-          <a href="https://www.linkedin.com/in/abudu-abiodun-751b12b0/" target="_blank">LinkedIn</a>
+        <div className="flex flex-wrap pt-10" id="social-link">
+          <a className="lg:w-auto w-full m-2 text-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10" href="https://web.facebook.com/sulenchy/" target="_blank">Facebook</a>
+          <a className="lg:w-auto w-full m-2 text-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10" href="https://github.com/sulenchy" target="_blank">Github</a>
+          <a className="lg:w-auto w-full m-2 text-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10" href="https://twitter.com/ababsul" target="_blank">Twitter</a>
+          <a className="lg:w-auto w-full m-2 text-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10" href="https://www.linkedin.com/in/abudu-abiodun-751b12b0/" target="_blank">LinkedIn</a>
         </div>
       </section>
       <footer id="footer" className="bg-blue-400 shadow-lg  z-50 w-full text-white text-center">
