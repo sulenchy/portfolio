dd4f477bb93692d953de161fdff02ad341d6cdfa
sulenchy <sulaimanabiodun172@gmail.com>
2021-01-09 09:17:05 +0100

    add info about my current engagement

--- src/main.tsx
+++ src/main.tsx
@@ -28,19 +28,21 @@ export default function main() {
         <main className="lg:w-1/2 mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 align-center">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Hey, I'm Sulenchy. I code with </span>
-              <span className="block text-indigo-600 xl:inline">js and ts</span>
+              <span className="block xl:inline">Hey, I'm Sulenchy. </span>
             </h1>
             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
-            I am a meticulous software engineer who loves using technology to be creative, innovative and solve real problems. I have 3+ years of experience in software engineering. I love getting things better.
+            I am a meticulous software engineer who loves using technology to be creative, innovative and solve real problems. I have 3+ years of experience in software engineering. I am passionate about building from scratch and getting things better.
             </p>
             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
-            I am proficient at React, Node, ExpressJs, Postgres, Rest API.
+            I am proficient at React, Node, ExpressJs, Postgres, Rest API, Javascript.
+            </p>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
+              I am an employee of <a className="text-blue-300 hover:text-blue-700" href="https://www.andela.com" target="_blank">Andela</a> working with <a className="text-blue-300 hover:text-blue-700" href="https://www.snaps.io" target="_blank">Snaps</a>. For your info, Snaps is a client of Andela. Most times in Snaps, I work on the frontend app.
             </p>
 
             <div className="mt-5 sm:mt-8 sm:flex justify-center">
               <div className="rounded-md shadow">
-                <a href="mailto:sulaimanabiodun172@gmail.com?subject=Hi%20,%20can%20we%20work%20together&body=Add%20your%20content%20here.%20Please,%20be%20brief." className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                <a href="mailto:sulaimanabiodun172@gmail.com?subject=Hi%20,%20can%20we%20work%20together&body=Tell%20me%20more%20how%20we%20can%20collaborate.%20Please,%20be%20brief." className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
                   Mail me
                 </a>
               </div>
