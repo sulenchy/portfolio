11ef205a224f2252cc2bf1d9e2948dfd82c51546
sulenchy <sulaimanabiodun172@gmail.com>
2021-09-25 16:07:53 +0100

    add skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BeakerIcon, ShieldCheckIcon } from '@heroicons/react/solid'
 import avatar from  './avatar.jpg';
 import './App.css';
 
@@ -11,16 +12,18 @@ function App() {
           <span className="md:hidden">breadcrumb</span>
         </div>
         <div className="w-full md:w-2/3 hidden md:block">
-          <nav className="list-none text-left  md:flex justify-end">
-            <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out">Skills</li>
-            <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out">Experiences</li>
-            <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out">Projects</li>
-            <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out">Contact</li>
+          <nav className="">
+            <ul className="list-none text-left  md:flex justify-end">
+              <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out"><a href="#skills">Skills</a></li>
+              <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out"><a href="#experiences">Experiences</a></li>
+              <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out"><a href="#projects">Projects</a></li>
+              <li className="p-5 rounded-r-2xl hover:bg-blue-500 cursor-pointer transition delay-150 duration-300 ease-out"><a href="#contacts">Contact</a></li>
+            </ul>
           </nav>
         </div>
       </header>
       <article>
-        <section id="about-me" className="w-full h-screen bg-about-image bg-repeat-round mix-blend-multiply">
+        <section id="about-me" className="w-full h-screen bg-about-image pt-20 bg-repeat-round mix-blend-multiply">
           <div className="w-60 h-60 absolute inset-24">
             <img className="rounded-full opacity-50" src={avatar} alt=""/>
             <span className="animate-ping absolute inline-flex h-10 w-10 rounded-full bg-blue-900 opacity-75"></span>
@@ -33,25 +36,38 @@ function App() {
             </div>
           </div>
         </section>
-        <section id="#skills" className="w-full h-screen">
-          <header>
-            <img src="" alt=""/>
-            <span>Skills</span>
+        <section id="skills" className="w-full h-screen pt-20 flex flex-col justify-center items-center">
+          <header className="flex justify-center items-center">
+            <ShieldCheckIcon className="h-10 w-10 text-blue-500" />
+            <span className="pl-5 text-3xl text-opacity-95">Skills</span>
           </header>
+          <nav className="pl-10 md:w-2/3 flex-col mx-auto">
+            <ul className="list-square text-2xl font-light leading-10">
+              <li className="p-3 hover:bg-blue-500 hover:text-blue-200 transition delay-150 duration-300 ease-out">Programming with Javascript, Typescript and C#</li>
+              <li className="p-3 hover:bg-blue-500 hover:text-blue-200 transition delay-150 duration-300 ease-out">Web development using ReactJS and Redux</li>
+              <li className="p-3 hover:bg-blue-500 hover:text-blue-200 transition delay-150 duration-300 ease-out">Restful microservices development using NodeJS</li>
+              <li className="p-3 hover:bg-blue-500 hover:text-blue-200 transition delay-150 duration-300 ease-out">Web scrapping using scrapping packages such as pupetteer</li>
+              <li className="p-3 hover:bg-blue-500 hover:text-blue-200 transition delay-150 duration-300 ease-out">Mobile development using Java and React-Native</li>
+              <li className="p-3 hover:bg-blue-500 hover:text-blue-200 transition delay-150 duration-300 ease-out">Containerization with Docker</li>
+            </ul>
+          </nav>
         </section>
-        <section id="#experiences" className="w-full h-screen">
+        <section id="experiences" className="w-full h-screen">
           <header>
-            <img src="" alt=""/>
+            <BeakerIcon className="h-5 w-5 text-blue-500" />
             <span>Experiences</span>
           </header>
+          <div>
+            
+          </div>
         </section>
-        <section id="#projects" className="w-full h-screen">
+        <section id="projects" className="w-full h-screen">
           <header>
             <img src="" alt=""/>
             <span>Projects</span>
           </header>
         </section>
-        <section id="#contacts" className="w-full h-screen">
+        <section id="contacts" className="w-full h-screen">
           <header>
             <img src="" alt=""/>
             <span>Contacts</span>
