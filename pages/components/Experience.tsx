7ed51941d503d06a6692e1c45e00d443f2ed66d8
sulenchy <sulaimanabiodun172@gmail.com>
2022-09-29 05:44:49 +0800

    initial commit

--- pages/components/Experience.tsx
+++ pages/components/Experience.tsx
@@ -1,6 +1,6 @@
 import useSWR from 'swr';
 import { BriefcaseIcon } from '@heroicons/react/solid';
-import styles from './Skills.module.scss'
+import styles from './Experience.module.scss'
 import { skills } from '../../utils/index'
 import { IExpr } from '../../utils/types';
 
@@ -13,39 +13,47 @@ function Experience() {
   if (!data) return <div>Loading...</div>
 
   return (
-    <div id="experience" className={styles.skillsWrapper}>
+    <div id="experience" className={styles.experienceWrapper}>
       <header className={styles.header}>
         <BriefcaseIcon className={styles.shieldCheckIcon}/>
         <span className={styles.title}>Experience</span>
       </header>
-      <main>
-        {
-          Object.values(data).map((expr: IExpr, index) => (
-            <div key={ `${expr.companyName}-${index}` } id={ expr.companyName } className="mb-5 p-5 md:p-10 md:flex hover:shadow text-left">
-              <div>
-                <h3>{ expr.companyName }</h3>
-                <h4>{ expr.location }</h4>
-                <span>{ expr.startDate } - { expr.endDate ? expr.endDate : 'current' }</span>
-                <span><a href={`http://${expr.links.companyUrl}`} target="_blank" rel="noreferrer">{expr.links.companyUrl}</a></span>
+      <div className={styles.slider}>
+        <header>
+          <ul className={styles.listWrapper}>
+            <li><a className={styles.listItem} href="#slide-1">1</a></li>
+            <li><a className={styles.listItem} href="#slide-2">2</a></li>
+          </ul>
+        </header>
+        <div className={styles.slides}>
+          {
+            Object.values(data).map((expr: IExpr, index) => (
+              <div key={ `${expr.companyName}-${index}` } id={ `slide-${index+1}` } className={styles.slide}>
+                <div className={styles.col}>
+                  <h3>{ expr.companyName }</h3>
+                  <h4>{ expr.location }</h4>
+                  <span>{ expr.startDate } - { expr.endDate ? expr.endDate : 'current' }</span>
+                  <span><a href={`http://${expr.links.companyUrl}`} target="_blank" rel="noreferrer">{expr.links.companyUrl}</a></span>
+                </div>
+                <div className={styles.col}>
+                  <div>
+                    <span>Role</span>
+                    <p>{expr.role}</p>
+                  </div>
+                  <div>
+                    <span>Accomplishment</span>
+                    <p>{expr.achievement}</p>
+                  </div>
+                  <div>
+                    <span>Skills, Tools and specializations</span>
+                    <p>{expr.skillsDemonstrated.map((skill: string, index) => (<span key={`${skill}-${index}`}>{skills[skill].name}, </span>))}</p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <div>
-                  <span>Role</span>
-                  <p>{expr.role}</p>
-                </div>
-                <div>
-                  <span>Accomplishment</span>
-                  <p>{expr.achievement}</p>
-                </div>
-                <div>
-                  <span>Skills, Tools and specializations</span>
-                  <p>{expr.skillsDemonstrated.map((skill: string, index) => (<span key={`${skill}-${index}`}>{skills[skill].name}, </span>))}</p>
-                </div>
-              </div>
-            </div>
-          ))
-        }
-      </main>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
